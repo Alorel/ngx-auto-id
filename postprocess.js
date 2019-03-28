7fa7a85da3139a1db12b3577bcd6ac3acdda4d91
Arturas <a.molcanovas@gmail.com>
2019-03-28 14:23:24 +0000

    fix(build): Sync description, repository, license fields

--- postprocess.js
+++ postprocess.js
@@ -6,7 +6,11 @@ const path = join(__dirname, 'dist', 'ngx-auto-id', 'package.json');
 const json = JSON.parse(fs.readFileSync(path, 'utf8'));
 delete json.dependencies;
 json.types = json.typings;
-json.version = require('./package.json').version;
-json.keywords = require('./package.json').keywords;
+
+const rootPkgJson = require('./package');
+const syncedKeys = ['version', 'keywords', 'description', 'repository', 'license'];
+for (const k of syncedKeys) {
+  json[k] = rootPkgJson[k];
+}
 
 fs.writeFileSync(path, JSON.stringify(json, null, 2));
