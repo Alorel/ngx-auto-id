a1397a1b13929770e92a986c78ca7b11d84f0f03
Arturas <a.molcanovas@gmail.com>
2019-03-28 14:17:36 +0000

    fix(build): Sync keywords with final package.json

--- postprocess.js
+++ postprocess.js
@@ -7,5 +7,6 @@ const json = JSON.parse(fs.readFileSync(path, 'utf8'));
 delete json.dependencies;
 json.types = json.typings;
 json.version = require('./package.json').version;
+json.keywords = require('./package.json').keywords;
 
 fs.writeFileSync(path, JSON.stringify(json, null, 2));
